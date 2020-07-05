67fc59faba882f786364ece455e76ad86573cc39
Guille <guillermo.villois@gmail.com>
2020-07-05 12:52:34 -0300

    update sheet

--- main.js
+++ main.js
@@ -15,5 +15,39 @@ client.authorize(function (err, tocken) {
         return;
     } else {
         console.log('Connected')
+        gsrun(client)
     }
 })
+
+async function gsrun(cl) {
+    const gsapi = google.sheets({ version: 'v4', auth: cl });
+
+    const opt = {
+        spreadsheetId: '11RwNdoDwYEkmo2VAgOjziiXOhuGDvRk51BmyAzDskiA',
+        range: 'hoja!A1:B5'
+    };
+
+    let data = await gsapi.spreadsheets.values.get(opt);
+
+    let dataArray = data.data.values;
+    dataArray = dataArray.map((r) => {
+        while (r.length < 2) {
+            r.push('');
+        }
+        return r;
+    })
+    console.log(dataArray);
+    let newDataArray = dataArray.map(function (r) {
+        r.push(r[0] + ' - ' + r[1]);
+        return r;
+    });
+    console.log(newDataArray);
+    const updateOptions = {
+        spreadsheetId: '11RwNdoDwYEkmo2VAgOjziiXOhuGDvRk51BmyAzDskiA',
+        range: 'hoja!J3',
+        valueInputOption: 'USER_ENTERED',
+        resource: { values: newDataArray }
+    }
+    let res = await gsapi.spreadsheets.values.update(updateOptions);
+    console.log(res)
+}
