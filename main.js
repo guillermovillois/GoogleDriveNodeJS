ac35d2c928282b0a9684ec9d34ec6c9ed4902165
Guille <guillermo.villois@gmail.com>
2020-07-05 11:43:52 -0300

    segundo commmit

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 const { google } = require('googleapis');
+const keys = require('./keys.json');
+
+
+const client = new google.auth.JWT(
+    keys.client_email,
+    null,
+    keys.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets']
+);
+
+client.authorize(function (err, tocken) {
+    if (err) {
+        console.log(err);
+        return;
+    } else {
+        console.log('Connected')
+    }
+})
