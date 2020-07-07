4700401e5bfd8910a0968aa9f19c9e64ea2bf9df
Guille <guillermo.villois@gmail.com>
2020-07-06 21:31:49 -0300

    final workbook

--- excelmgnt.js
+++ excelmgnt.js
@@ -1,6 +1,18 @@
 const xlsx = require('xlsx');
 
-var workbook = xlsx.readFile('data.xlsx');
-
-console.log(workbook.Strings);
+var workbook = xlsx.readFile('data.xlsx', {});
+var sheet = workbook.Sheets['data']
+var data = xlsx.utils.sheet_to_json(sheet)
+var newData = data.map((record) => {
+    record.FY19 = record.GS + record.GC;
+    delete record.GS;
+    delete record.GC;
+    return record;
+})
+console.log(newData);
 console.log(workbook.SheetNames)
+
+let newWorkBook = xlsx.utils.book_new();
+let newWS = xlsx.utils.json_to_sheet(newData);
+xlsx.utils.book_append_sheet(newWorkBook, newWS, 'newData')
+xlsx.writeFile(newWorkBook, 'NewDATAFILE.xlsx')
